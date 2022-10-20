2fe99aeb71b9ade382bf66a278a966840e3ebe9c
eflerrr <cool.yan_sv@mail.ru>
2022-10-20 10:36:47 +0300

    l2_task_6: final pre-release

--- lab_2/l2_draft_6.c
+++ lab_2/l2_draft_6.c
@@ -3,28 +3,18 @@
 #include <string.h>
 
 
-typedef struct path_matches1{
-    int checker;
-    int matches;
-    char* path;
-} path_matches1;
-
-
-path_matches1* matches_in_files(char str[], int count_files, ...){
+int matches_in_files(char str[], int count_files, ...){
     va_list l;
     va_start(l, count_files);
     int len = strlen(str);
     char c;
-    path_matches1 res[count_files-1];
 
     for(int i = 0; i < count_files; i++){
         char* file_name = va_arg(l, char*);
         FILE* fin = fopen(file_name, "r");
         if(!fin){
             printf("File Error: could not open file\n");
-            res[i].checker = 0;
-            res[i].matches = 0;
-            res[i].path = file_name;
+            return 0;
         }
         int index = 0;
         int good_symbols = 0;
@@ -49,23 +39,17 @@ path_matches1* matches_in_files(char str[], int count_files, ...){
                 }
             }
         }
-//        printf("Substring '%s' in file '%s' ---> %d matches\n", str, file_name, matches);
-        res[i].checker = 1;
-        res[i].matches = matches;
-        res[i].path = file_name;
+        printf("Substring '%s' in file '%s' ---> %d matches\n", str, file_name, matches);
         fclose(fin);
     }
-    return res;
+    return 1;
 }
 
 
 int main(int argc, char *argv[]){
     printf("\n");
-    int count_files = 3;
     char path1[] = "D:\\JetBrains\\tester_files_(yan)\\l2_task_6_tester1.txt";
     char path2[] = "D:\\JetBrains\\tester_files_(yan)\\l2_task_6_tester2.txt";
     char path3[] = "D:\\JetBrains\\tester_files_(yan)\\l2_task_6_tester3.txt";
-    path_matches1 res[count_files-1];
-    res = matches_in_files("33", count_files, path1, path2, path3);
-    printf("%d", res[1].matches);
+    matches_in_files("33", 3, path1, path2, path3);
 }
--- lab_2/l2_task_6.c
+++ lab_2/l2_task_6.c
@@ -3,7 +3,14 @@
 #include <string.h>
 
 
-int matches_in_files(char str[], int count_files, ...){
+typedef struct path_matches{
+    int checker;
+    int matches;
+    char* path;
+} path_matches;
+
+
+void matches_in_files(path_matches res[], char str[], int count_files, ...){
     va_list l;
     va_start(l, count_files);
     int len = strlen(str);
@@ -14,7 +21,10 @@ int matches_in_files(char str[], int count_files, ...){
         FILE* fin = fopen(file_name, "r");
         if(!fin){
             printf("File Error: could not open file\n");
-            return 0;
+            res[i].checker = 0;
+            res[i].matches = 0;
+            res[i].path = file_name;
+            continue;
         }
         int index = 0;
         int good_symbols = 0;
@@ -39,17 +49,31 @@ int matches_in_files(char str[], int count_files, ...){
                 }
             }
         }
-        printf("Substring '%s' in file '%s' ---> %d matches\n", str, file_name, matches);
+        res[i].checker = 1;
+        res[i].matches = matches;
+        res[i].path = file_name;
         fclose(fin);
     }
-    return 1;
 }
 
 
 int main(int argc, char *argv[]){
     printf("\n");
+
+    int count_files = 3;
     char path1[] = "D:\\JetBrains\\tester_files_(yan)\\l2_task_6_tester1.txt";
     char path2[] = "D:\\JetBrains\\tester_files_(yan)\\l2_task_6_tester2.txt";
     char path3[] = "D:\\JetBrains\\tester_files_(yan)\\l2_task_6_tester3.txt";
-    matches_in_files("33", 3, path1, path2, path3);
+    char str[] = "33";
+
+    path_matches res[count_files];
+    matches_in_files(res, str, count_files, path1, path2, path3);
+    for(int i = 0; i < count_files; i++){
+        if(res[i].checker){
+            printf("Substring '%s' in file '%s' ---> %d matches\n", str, res[i].path, res[i].matches);
+        }
+        else{
+            printf("Substring '%s' in file '%s' ---> could not open a file\n", str, res[i].path);
+        }
+    }
 }
